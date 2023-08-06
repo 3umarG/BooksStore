bbb827bbb675550bacf26ada231f0a9539c6ad4e
OmarGomaaFCi <omargomaafcitu@gmail.com>
2023-08-06 13:02:56 +0200

    Pla

--- src/main/java/com/books/BooksStore/BooksStoreApplication.java
+++ src/main/java/com/books/BooksStore/BooksStoreApplication.java
@@ -1,13 +1,23 @@
 package com.books.BooksStore;
 
+import com.books.service.BooksService;
+import lombok.AllArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BooksStoreApplication {
+@AllArgsConstructor
+public class BooksStoreApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BooksStoreApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BooksStoreApplication.class, args);
+    }
 
+    private final BooksService service;
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
 }
--- src/main/java/com/books/models/Book.java
+++ src/main/java/com/books/models/Book.java
@@ -13,36 +13,4 @@ public class Book {
     private final double price;
     private final int quantity;
 
-//    public Book(int id, String title, String author, String description, double price, int quantity) {
-//        this.id = id;
-//        this.title = title;
-//        this.author = author;
-//        this.description = description;
-//        this.price = price;
-//        this.quantity = quantity;
-//    }
-//
-//    public int getId() {
-//        return id;
-//    }
-//
-//    public String getTitle() {
-//        return title;
-//    }
-//
-//    public String getAuthor() {
-//        return author;
-//    }
-//
-//    public String getDescription() {
-//        return description;
-//    }
-//
-//    public double getPrice() {
-//        return price;
-//    }
-//
-//    public int getQuantity() {
-//        return quantity;
-//    }
 }
